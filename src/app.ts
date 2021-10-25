250cde944acbacf6ac1970335954617d640acbc0
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2021-10-25 14:28:01 +0200

    Create basic game manager

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,48 @@
-import Board from "./game/Board";
+import Game from "./game/Game";
+import levels from "./data";
+const prompt = require("prompt-sync")();
 
-const board: Board = new Board(8, 8);
-board.createBoard();
-board.addMines(10);
-board.configureFields();
-board.render();
+function intro(): void {
+    console.log(`
+█▀▄▀█ █ █▄░█ █▀▀ █▀ █░█░█ █▀▀ █▀▀ █▀█ █▀▀ █▀█
+█░▀░█ █ █░▀█ ██▄ ▄█ ▀▄▀▄▀ ██▄ ██▄ █▀▀ ██▄ █▀▄
+
+█▀▄▀█ ▄▀█ █▀▄ █▀▀   █▄▄ █▄█  
+█░▀░█ █▀█ █▄▀ ██▄   █▄█ ░█░  
+
+█▀▄▀█ █ █▄▀ █▀█ █░░ ▄▀█ ░░█   █▀ ▀█ █▄█ █▀▄▀█ █▀▀ ▀█ █░█ █▄▀
+█░▀░█ █ █░█ █▄█ █▄▄ █▀█ █▄█   ▄█ █▄ ░█░ █░▀░█ █▄▄ █▄ █▄█ █░█
+    `);
+}
+
+function createGame(): Game {
+    let difficult = prompt("Podaj poziom trudności (1: łatwy, 2: średni, 3: trudny): ");
+    let cd;  // Current Difficult
+
+    switch(difficult) {
+        case "1":
+            cd = levels["easy"];
+            break;
+
+        case "2":
+            cd = levels["medium"];
+            break;
+
+        case "3":
+            cd = levels["hard"];
+            break;
+
+        default:
+            cd = levels["easy"];
+            break;
+    }
+
+    return new Game(cd.size.width, cd.size.height, cd.minesCount);
+}
+
+function main(): void {
+    intro();
+    const game: Game = createGame();
+}
+
+main();
