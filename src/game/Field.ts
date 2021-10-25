67ef33144b5500029ba4e26731894a70693ed95d
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2021-10-25 12:38:31 +0200

    Add discover method

--- src/game/Field.ts
+++ src/game/Field.ts
@@ -1,7 +1,13 @@
 export default class Field {
     public value: number;
+    public isDiscover: boolean;
 
     constructor(value: number) {
         this.value = value;
+        this.isDiscover = false;
+    }
+
+    discover(): void {
+        this.isDiscover = true;
     }
 }
